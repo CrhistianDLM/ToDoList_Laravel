061959fb3c9a4e20a1a89abfc70562c19b905075
lucho2024 <luishm2024@gmail.com>
2019-10-29 17:02:40 -0500

    cambios

---
+++ resources/views/prueba.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <style >
+      .cajita{
+        width: 400px;
+        height: 400px;
+        background: black;
+      }
+    </style>
+  </head>
+  <body>
+
+    <h1>Hola</h1>
+    <div class="cajita" >
+
+    </div>
+
+  </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,10 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('prueba');
+});
+Route::get('/bffhh', function () {
+    return view('prueba');
 });
 
 Auth::routes();
