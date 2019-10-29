e77b1cef7051e7eba2b454292385265df36eab0e
golden659 <velascocristian309@gmail.com>
2019-10-29 17:03:10 -0500

    clase1

---
+++ resources/views/vistass.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <style>
+    .cajita{
+      width: 500px;
+      height: 500px;
+      background: black;
+    }
+
+    </style>
+  </head>
+  <body>
+    <h1>hola mundo</h1>
+    <div class="cajita">
+
+    </div>
+  </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,10 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('vistass');
+});
+Route::get('/holamundo', function () {
+    return view('vistass');
 });
 
 Auth::routes();
